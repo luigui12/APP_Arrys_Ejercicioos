b9a524a955c35c38404c3c5b0d177a16100d47b3
jofremaza37 <jofre.maza@pucese.edu.ec>
2019-11-27 13:03:38 -0500

    Practica Array Lista

--- APP_Arrys_Ejercicioos/Form1.Designer.cs
+++ APP_Arrys_Ejercicioos/Form1.Designer.cs
@@ -28,20 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.numero = new System.Windows.Forms.TextBox();
+            this.agregar = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.promedio1 = new System.Windows.Forms.TextBox();
+            this.suma = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // numero
+            // 
+            this.numero.Location = new System.Drawing.Point(135, 82);
+            this.numero.Name = "numero";
+            this.numero.Size = new System.Drawing.Size(100, 20);
+            this.numero.TabIndex = 0;
+            // 
+            // agregar
+            // 
+            this.agregar.Location = new System.Drawing.Point(28, 124);
+            this.agregar.Name = "agregar";
+            this.agregar.Size = new System.Drawing.Size(75, 23);
+            this.agregar.TabIndex = 1;
+            this.agregar.Text = "Agregar";
+            this.agregar.UseVisualStyleBackColor = true;
+            this.agregar.Click += new System.EventHandler(this.agregar_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.Location = new System.Drawing.Point(135, 124);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(80, 23);
+            this.resultado.TabIndex = 2;
+            this.resultado.Text = "Mostar Re.";
+            this.resultado.UseVisualStyleBackColor = true;
+            this.resultado.Click += new System.EventHandler(this.resultado_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(25, 82);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 13);
+            this.linkLabel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ingrese un numero:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Promedio: ";
+            // 
+            // promedio1
+            // 
+            this.promedio1.Location = new System.Drawing.Point(283, 206);
+            this.promedio1.Name = "promedio1";
+            this.promedio1.Size = new System.Drawing.Size(100, 20);
+            this.promedio1.TabIndex = 6;
+            // 
+            // suma
+            // 
+            this.suma.Location = new System.Drawing.Point(283, 241);
+            this.suma.Name = "suma";
+            this.suma.Size = new System.Drawing.Size(100, 20);
+            this.suma.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Suma:";
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(24, 163);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(175, 186);
+            this.lista.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(88, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(239, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ejercicio de Array";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(306, 291);
+            this.ClientSize = new System.Drawing.Size(414, 395);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.suma);
+            this.Controls.Add(this.promedio1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.agregar);
+            this.Controls.Add(this.numero);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox numero;
+        private System.Windows.Forms.Button agregar;
+        private System.Windows.Forms.Button resultado;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox promedio1;
+        private System.Windows.Forms.TextBox suma;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- APP_Arrys_Ejercicioos/Form1.cs
+++ APP_Arrys_Ejercicioos/Form1.cs
@@ -12,9 +12,56 @@ namespace APP_Arrys_Ejercicioos
 {
     public partial class Form1 : Form
     {
+        private int conta = 0;
+        private const int Max = 100;
+        private double[] vector = new double[Max];
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void agregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conta < Max)
+                {
+                    vector[conta] = int.Parse(this.numero.Text);
+                }
+                conta++;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void resultado_Click(object sender, EventArgs e)
+        {
+            for (int i=0;i<conta;i++)
+            {
+                this.lista.Items.Add(vector[i]);
+            }
+            this.suma.Text = sumatoria().ToString();
+            this.promedio1.Text = promedio().ToString();
+        }
+
+        private double sumatoria() {
+            double suma = 0;
+            for (int i=0;i<conta;i++)
+            {
+                suma = suma + vector[i];
+            }
+            return suma;
+        }
+        private double promedio()
+        {
+            return (sumatoria()/conta);
         }
     }
 }
